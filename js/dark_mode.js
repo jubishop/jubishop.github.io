721cc222a89742462fa971713cd72e51114c4a8f
Justin Bishop <jubishop@gmail.com>
2020-08-20 16:57:08 -0700

    fully working dark mode toggle

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,8 +1,51 @@
-dark_mode = !!Cookies.get("dark_mode");
-console.log(Cookies.get());
-console.log(dark_mode);
-function toggleDarkMode() {
-  dark_mode = !dark_mode;
-  Cookies.set("dark_mode", dark_mode);
-  console.log(dark_mode);
+class DarkMode {
+  static domLoaded() {
+    this.DarkBG =
+      getComputedStyle(document.body).getPropertyValue("--dark-bg-color");
+    this.LightBG =
+      getComputedStyle(document.body).getPropertyValue("--light-bg-color");
+    this.DarkText =
+      getComputedStyle(document.body).getPropertyValue("--dark-text-color");
+    this.LightText =
+      getComputedStyle(document.body).getPropertyValue("--light-text-color");
+
+    this.updateStyles();
+
+    var time_elem = document.getElementById("time");
+    time_elem.checked = this.getDarkMode();
+    time_elem.addEventListener("click", () => this.toggleDarkMode());
+  }
+
+  static setDarkMode(dark_mode) {
+    Cookies.set("dark_mode", dark_mode);
+  }
+
+  static toggleDarkMode() {
+    this.setDarkMode(!this.getDarkMode());
+    this.updateStyles();
+  }
+
+  static getDarkMode() {
+    return (Cookies.get("dark_mode") !== "false");
+  }
+
+  static updateStyles() {
+    var dark_mode = this.getDarkMode();
+    document.body.style.setProperty(
+      "--main-bg-color",
+      dark_mode ? this.DarkBG : this.LightBG);
+    document.body.style.setProperty(
+      "--main-text-color",
+      dark_mode ? this.DarkText : this.LightText);
+
+    if (dark_mode) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    }
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => DarkMode.domLoaded());
