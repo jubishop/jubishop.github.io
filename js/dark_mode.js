4c524334987637ebbeb4804305a81c9a73d056c8
Justin Bishop <jubishop@gmail.com>
2020-08-20 15:38:12 -0700

    testing out js

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,3 +1,8 @@
+dark_mode = !!Cookies.get("dark_mode");
+console.log(Cookies.get());
+console.log(dark_mode);
 function toggleDarkMode() {
-  console.log("toggling");
+  dark_mode = !dark_mode;
+  Cookies.set("dark_mode", dark_mode);
+  console.log(dark_mode);
 }
